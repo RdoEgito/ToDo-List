dfa0910f1c888b0010f657ab3fde8ce8dbb18b9d
RdoEgito <33696073+RdoEgito@users.noreply.github.com>
2021-03-29 16:24:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 let items = []
 
-function addItem(itemName, itemValue) {
+function addItem(itemName, itemValue, itemStatus) {
     items.sort()
     itemName = itemName || getItemName(items)
     itemValue = itemValue || ""
+    itemStatus = itemStatus || ""
 
     const checkBox = document.createElement("input")
     checkBox.classList.add(itemName)
+    checkBox.classList.add(itemStatus)
     checkBox.setAttribute("type", "checkbox")
     checkBox.setAttribute("onclick", "checkBoxClicked(this)")
 
     const textBox = document.createElement("input")
     textBox.classList.add(itemName)
+    textBox.classList.add(itemStatus)
+    textBox.classList.add("itemText")
     textBox.setAttribute("type", "text")
     textBox.setAttribute("value", itemValue)
     textBox.setAttribute("oninput", "setCookie(this.classList[0], this.value, 800)")
 
     const closeButton = document.createElement("button")
     closeButton.classList.add(itemName)
+    closeButton.classList.add(itemStatus)
     closeButton.classList.add("closeButton")
     closeButton.innerText = "✖️"
     closeButton.setAttribute("onclick", "deleteItem(this)")
@@ -25,6 +30,7 @@ function addItem(itemName, itemValue) {
     const divItem = document.createElement("div")
     divItem.classList.add(itemName)
     divItem.classList.add("item")
+    divItem.classList.add(itemStatus)
     divItem.appendChild(checkBox)
     divItem.appendChild(textBox)
     divItem.appendChild(closeButton)
@@ -51,18 +57,24 @@ function deleteItem(e) {
 }
 
 function checkBoxClicked(e) {
-    const containerDiv = document.querySelector("div." + e.classList[0])
+    const itemName = e.classList[0]
+    const containerDiv = document.querySelector("div." + itemName)
+    const textValue = containerDiv.querySelector("input.itemText").nodeValue
     const divActiveItems = document.querySelector("#activeItems")
     const divInactiveItems = document.querySelector("#inactiveItems")
 
     if (e.checked) {
-        document.querySelectorAll("." + e.classList[0]).forEach(item => item.classList.add("inactive"))
+        document.querySelectorAll("." + itemName).forEach(item => item.classList.add("inactive"))
         divInactiveItems.appendChild(containerDiv)
+        setCookie(itemName, textValue, "inactive", 800)
     } else {
-        document.querySelectorAll("." + e.classList[0]).forEach(item => item.classList.remove("inactive"))
+        document.querySelectorAll("." + itemName).forEach(item => item.classList.remove("inactive"))
         divActiveItems.appendChild(containerDiv)
+        setCookie(itemName, textValue, "active", 800)
     }
 }
+
+let cookieItem, cookieText, cookieStatus
 
 function getSavedItems() {
     let cookie = document.cookie
@@ -72,17 +84,25 @@ function getSavedItems() {
         const cookieName = item.split("=")[0].trim()
         const cookieValue = item.split("=")[1].trim()
 
-        let pattern = new RegExp(/item\d/)
-        console.log(pattern.test(cookieName), cookieName)
-        if (pattern.test(cookieName)) addItem(cookieName, cookieValue)
+        let patternItem = new RegExp(/item\d/)
+        let patternStatus = new RegExp(/itemStatus\d/)
+            // console.log(patternItem.test(cookieName), cookieName)
+        if (patternItem.test(cookieName)) {
+            cookieItem = cookieName
+            cookieText = cookieValue
+        } else if (patternStatus.test(cookieName)) {
+            cookieStatus = cookieValue
+
+            addItem(cookieItem, cookieText, cookieStatus)
+        }
     })
 }
 
-function setCookie(cname, cvalue, exdays) {
+function setCookie(cname, cvalue, cstatus, exdays) {
     let d = new Date()
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
     let expires = "expires=" + d.toUTCString()
-    let cookie = cname + "=" + cvalue + ";" + expires + ";path=/"
+    let cookie = cname + "=" + cvalue + ";" + cname + "Status=" + cstatus + ";" + expires + ";path=/"
     document.cookie = cookie
     console.log(cookie)
 }
